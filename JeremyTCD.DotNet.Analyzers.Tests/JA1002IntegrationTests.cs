4f8cd8c0d407c1b965a4a139151af8b3ef618611
JeremyTCD <jeremytanchongdao@gmail.com>
2017-10-18 11:27:08 +0800

    Added integration tests for JA1002

---
+++ JeremyTCD.DotNet.Analyzers.Tests/JA1002IntegrationTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.CodeAnalysis;
+using Xunit;
+
+namespace JeremyTCD.DotNet.Analyzers.Tests
+{
+    public class JA1002IntegrationTests
+    {
+        private readonly SourcesHelper _sourcesHelper = new SourcesHelper(JA1002TestClassNamesMustEndWithAValidSuffix.DiagnosticId);
+        private readonly DiagnosticVerifier _diagnosticVerifier = new DiagnosticVerifier(new JA1002TestClassNamesMustEndWithAValidSuffix());
+
+        [Fact]
+        public void DiagnosticAnalyzer_DoesNotCreateDiagnosticsIfCompilationDoesNotContainTestClass()
+        {
+            _diagnosticVerifier.VerifyDiagnostics(_sourcesHelper.GetSourcesFolder());
+        }
+
+        [Fact]
+        public void DiagnosticAnalyzer_DoesNotCreateDiagnosticsIfTestClassNamesHaveValidSuffixes()
+        {
+            _diagnosticVerifier.VerifyDiagnostics(_sourcesHelper.GetSourcesFolder());
+        }
+
+        [Fact]
+        public void DiagnosticAnalyzer_CreatesDiagnosticWhenAllConditionsAreSatisfied()
+        {
+            DiagnosticResult expected = new DiagnosticResult
+            {
+                Id = JA1002TestClassNamesMustEndWithAValidSuffix.DiagnosticId,
+                Message = Strings.JA1002_MessageFormat,
+                Severity = DiagnosticSeverity.Warning,
+                Locations = new[] { new DiagnosticResultLocation("TestInvalidSuffix.cs", 5, 18) }
+            };
+
+            _diagnosticVerifier.VerifyDiagnostics(_sourcesHelper.GetSourcesFolder(), expected);
+        }
+    }
+}
