bb1ed347eeb475d65e5685864b80e8520a5cb72e
JeremyTCD <jeremytanchongdao@gmail.com>
2017-10-30 22:10:30 +0800

    Added some methods to MiscHelper

--- JeremyTCD.DotNet.Analyzers/MiscHelper.cs
+++ JeremyTCD.DotNet.Analyzers/MiscHelper.cs
@@ -1,4 +1,8 @@
-﻿namespace JeremyTCD.DotNet.Analyzers
+﻿using Microsoft.CodeAnalysis;
+using System;
+using System.Collections.Generic;
+
+namespace JeremyTCD.DotNet.Analyzers
 {
     public static class MiscHelper
     {
@@ -8,6 +12,43 @@
             sChars[0] = char.ToUpper(sChars[0]);
 
             return new string(sChars);
+        }
+
+        public static string RemoveNonAlphaNumericCharacters(this string str)
+        {
+            char[] arr = str.ToCharArray();
+
+            arr = Array.FindAll(arr, c => char.IsLetterOrDigit(c) || char.IsWhiteSpace(c));
+            return new string(arr);
+        }
+
+        public static string ToTitleCase(this string str)
+        {
+            var tokens = str.Split(new[] { " ", "-", "." }, StringSplitOptions.RemoveEmptyEntries);
+            for (var i = 0; i < tokens.Length; i++)
+            {
+                var token = tokens[i];
+                tokens[i] = token.FirstCharUpper();
+            }
+
+            return string.Join("", tokens);
+        }
+
+        public static IMethodSymbol FindImplementedMethod(IMethodSymbol methodSymbol)
+        {
+            foreach(INamedTypeSymbol implementedInterface in methodSymbol.ContainingType.Interfaces)
+            {
+                IEnumerable<IMethodSymbol> implementedMethods = implementedInterface.GetMembers().OfType<IMethodSymbol>();
+                foreach (IMethodSymbol method in implementedMethods)
+                {
+                    if(methodSymbol.ContainingType.FindImplementationForInterfaceMember(method) == methodSymbol)
+                    {
+                        return method;
+                    }
+                }
+            }
+
+            return null;
         }
     }
 }
